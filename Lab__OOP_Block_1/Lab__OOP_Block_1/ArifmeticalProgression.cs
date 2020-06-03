f4b0f04eab04a553f95a5c58ae74eab15ff4c9c0
Nikita3214 <45255917+Nikita3214@users.noreply.github.com>
2020-06-03 15:26:28 +0300

    Add files via upload

---
+++ Lab__OOP_Block_1/Lab__OOP_Block_1/ArifmeticalProgression.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Реалізувати клас, що представляє арифметичну прогресію.
+ * Передбачити індексатор для одержання i -го члена прогресії, методи введення/виведення, 
+ * знаходження суми n членів цієї прогресії.*/
+namespace Lab_3_OOP_Block_1
+{
+    class ArifmeticalProgression
+    {
+       
+        int n1;
+        int n2;
+
+        public ArifmeticalProgression(int n1_, int n2_)
+        {
+        n1 = n1_;
+        n2 = n2_;
+        }
+        public int this[int i]
+        {
+            get
+            {
+                return n1 + (n2 - n1) * (i - 1);
+            }
+        }
+            public int Suma(int i)
+        {
+            return (int)(((double)(n1 + this[i])/2)*i);
+        }
+
+    }
+        }
